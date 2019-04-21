d48e60e37e1dc18d34f373acc111c4794a4ef1a9
xyrin88 <xyrin88@gmail.com>
2019-04-21 03:08:14 +0200

    Bump version to v1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anthemtool',
-    version='1.1',
+    version='1.2',
     description='Unpacker for the Frostbite Engine based Anthem Game.',
     author='xyrin88',
     author_email='xyrin88@gmail.com',
